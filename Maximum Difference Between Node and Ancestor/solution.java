78291189c696849e77b9cbbedbb0a20f08d0179b
Snehal Mastud <smastud0@gmail.com>
2026-03-29 13:42:25 +0530

    Create solution.java

---
+++ Maximum Difference Between Node and Ancestor/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    private int ans;
+
+    public int maxAncestorDiff(TreeNode root) {
+        dfs(root, root.val, root.val);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int mi, int mx) {
+        if (root == null) {
+            return;
+        }
+        int x = Math.max(Math.abs(mi - root.val), Math.abs(mx - root.val));
+        ans = Math.max(ans, x);
+        mi = Math.min(mi, root.val);
+        mx = Math.max(mx, root.val);
+        dfs(root.left, mi, mx);
+        dfs(root.right, mi, mx);
+    }
+}
