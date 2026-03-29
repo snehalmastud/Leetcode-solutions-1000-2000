db19e6d4207fd0af3725e9bd2b0661f306210000
Snehal Mastud <smastud0@gmail.com>
2026-03-29 13:12:53 +0530

    Create solution.java

---
+++ Construct Binary Search Tree From Preorder Traversal/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return dfs(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode dfs(int[] preorder, int i, int j) {
+        if (i > j || i >= preorder.length) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[i]);
+        int left = i + 1, right = j + 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (preorder[mid] > preorder[i]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        root.left = dfs(preorder, i + 1, left - 1);
+        root.right = dfs(preorder, left, j);
+        return root;
+    }
+}
