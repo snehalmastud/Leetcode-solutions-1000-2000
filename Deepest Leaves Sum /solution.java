1139b69d61c7213b04695a5d246918ed5ba8883d
Snehal Mastud <smastud0@gmail.com>
2026-04-02 18:06:35 +0530

    Create solution.java

---
+++ Deepest Leaves Sum /solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int ans = 0;
+        while (!q.isEmpty()) {
+            ans = 0;
+            for (int n = q.size(); n > 0; --n) {
+                root = q.pollFirst();
+                ans += root.val;
+                if (root.left != null) {
+                    q.offer(root.left);
+                }
+                if (root.right != null) {
+                    q.offer(root.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
